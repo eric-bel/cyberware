49eaf26348e741d0edae1bb3216cfec3c193f7ab
eric bel <ericbelecov@gmail.com>
2021-11-23 08:14:44 +0400

    HeaderBtnInTerminal added

---
+++ src/components/Terminal/Header/HeaderInTerminal.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HeaderBtnInTerminal from "./HeaderBtn/HeaderBtnInTerminal";
+
+const Header = (props) => {
+  return (
+    <>
+      <HeaderBtnInTerminal />
+      {/* <HeaderBtn className="item2" title="Программа курса" />
+      <HeaderBtn className="item3" title="FAQ" />
+      <HeaderBtn className="item4" title="Контакты" /> */}
+    </>
+  );
+};
+
+export default React.memo(Header);
