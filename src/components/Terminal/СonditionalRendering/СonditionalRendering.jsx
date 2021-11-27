e7396d47ad479786116b94ae278804a3e475d7cd
eric bel <ericbelecov@gmail.com>
2021-11-27 21:25:58 +0400

    tsybulka help

--- src/components/Terminal/СonditionalRendering/СonditionalRendering.jsx
+++ src/components/Terminal/СonditionalRendering/СonditionalRendering.jsx
@@ -4,40 +4,56 @@ import About from "./About/About";
 import styles from "./СonditionalRendering.module.css";
 
 const СonditionalRendering = (props) => {
-  const [stateInputValue, setStateInputValue] = useState("");
-  const [wasEnterPressed, setEnterPressed] = useState(false);
+  const [state, setState] = useState({ inputValue: "", result: [] });
+  
+
+  const nandlePress = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      if (state.inputValue.toLowerCase() === "about") {
+        const res = [...state.result];
+        // res.push("About About About About");
+        res.push(<About />);
+        setState((prev) => ({
+          result: res,
+          inputValue: "",
+        }));
+      }
+      if (state.inputValue.toLowerCase() === "bla") {
+        const res = [...state.result];
+        res.push("bla bla bla bla");
+        setState((prev) => ({
+          result: res,
+          inputValue: "",
+        }));
+      }
+    }
+  };
+
+  const hadleInput = (e) => {
+    e.preventDefault();
+    setState((prev) => ({
+      ...prev,
+      inputValue: e.target.value,
+    }));
+  };
 
   return (
     <>
+      {state.result && state.result.length !== 0 && state.result.map((i) => <p>{i}</p>)}
       <form className={styles.form}>
         <label className={styles.label}> {"> "}Командная строка $ :</label>
-
         <input
           className={styles.inputgetcommand}
           name="getcommand"
           type="text"
           autoFocus={false}
           autoComplete="off"
-          value={stateInputValue}
-          onChange={(e) => {
-            e.preventDefault();
-            setStateInputValue(e.target.value);
-          }}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              e.preventDefault();
-              setEnterPressed(true);
-            }
-            if (wasEnterPressed) setEnterPressed(false);
-          }}
+          value={state.inputValue}
+          onChange={hadleInput}
+          onKeyDown={nandlePress}
         />
       </form>
-      {stateInputValue.toLowerCase() === "about" &&
-        wasEnterPressed &&
-        "about about about about about"}
-      {stateInputValue.toLowerCase() === "bla" &&
-        wasEnterPressed &&
-        "bla bla bla "}
     </>
   );
 };
